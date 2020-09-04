649b9556d3bb3fcb846f09dde84df93cd369f007
khetwal-om <khetwalom@gmail.com>
2020-09-04 16:54:27 +0530

    III Fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import Nav from './Nav';
 import Results from './Results';
 
-
+import requests from './requests'
 
 function App() {
+ const [selectedOption,setSelectedOption]=useState(requests.fetchTrending)
 
   
   return (
       <div className="App">
         <Header/>
-        <Nav /> 
+        <Nav setSelectedOption={setSelectedOption}/> 
  
          {/* resultS */}
-        <Results/>
+        <Results selectedOption={selectedOption}/>
 
       </div>
       
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -4,7 +4,7 @@ import TextTruncate from 'react-text-truncate';
 import ThumbUpSharp from '@material-ui/icons/ThumbUpSharp'
 
 const base_url="https://image.tmdb.org/t/p/original/";
-function VideoCard({movie}) {
+const  VideoCard =  ({movie}) =>{
     return (
         <div className="videocard">
             <img 
@@ -17,8 +17,14 @@ function VideoCard({movie}) {
                 truncateText=".."
                 text={movie.overview}
              />
-            <h2>{movie.title || movie.original_name}</h2>
-            <p> <ThumbUpSharp /> {movie.vote_count} </p>
+
+           <h2>{movie.title || movie.original_name}</h2>
+            
+            <p className="videocard__stats"> 
+                {movie.media_type&& `${movie.media_type}`}
+                {movie.release_date || movie.first_air_date}
+                
+                <ThumbUpSharp /> {movie.vote_count} </p>
         </div>
     )
 }
