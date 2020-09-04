6a0f226fe814d271abb6d2f63ff1a3dc62ac2308
khetwal-om <khetwalom@gmail.com>
2020-09-04 17:10:39 +0530

    4 final commit

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React,{forwardRef} from 'react'
 import './VideoCard.css'
 import TextTruncate from 'react-text-truncate';
 import ThumbUpSharp from '@material-ui/icons/ThumbUpSharp'
 
 const base_url="https://image.tmdb.org/t/p/original/";
-const  VideoCard =  ({movie}) =>{
+const  VideoCard =  forwardRef(({movie},ref) =>{
     return (
-        <div className="videocard">
+        <div ref={ref} className="videocard">
             <img 
             src={`${base_url}${movie.backdrop_path|| movie.poster_path} `}
             alt="sunny"
@@ -27,6 +27,6 @@ const  VideoCard =  ({movie}) =>{
                 <ThumbUpSharp /> {movie.vote_count} </p>
         </div>
     )
-}
+})
 
 export default VideoCard
